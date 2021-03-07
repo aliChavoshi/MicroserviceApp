1fb6c7c816cd6b33dbd724d2d1867801d6160b09
Ali Chavoshi <alichavoshii1372@gamil.com>
2021-03-07 10:03:18 +0330

    Seeding SQL Server Database with Entity Framework Core in Infrastructure Layer

--- src/Ordering/Ordering.Api/Program.cs
+++ src/Ordering/Ordering.Api/Program.cs
@@ -6,14 +6,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Infrastructure.Data;
 
 namespace Ordering.Api
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            await CreateAndSeedDatabase(host);
+            await host.RunAsync();
+
+            //CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -22,5 +29,22 @@ namespace Ordering.Api
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        private static async Task CreateAndSeedDatabase(IHost host)
+        {
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+            try
+            {
+                var orderContext = services.GetRequiredService<OrderContext>();
+                await OrderContextSeed.SeedAsync(orderContext, loggerFactory);
+            }
+            catch (Exception exception)
+            {
+                var logger = loggerFactory.CreateLogger<Program>();
+                logger.LogError(exception.Message);
+            }
+        }
     }
 }
---
+++ src/Ordering/Ordering.Infrastructure/Data/OrderContextSeed.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Ordering.Core.Entities;
+
+namespace Ordering.Infrastructure.Data
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext context, ILoggerFactory loggerFactory, int? retry = 0)
+        {
+            var retryForAvailability = retry.Value;
+            try
+            {
+                await context.Database.MigrateAsync();
+                if (!await context.Order.AnyAsync())
+                {
+                    await context.Order.AddRangeAsync(GetPerConfigOrders());
+                    await context.SaveChangesAsync();
+                }
+            }
+            catch (Exception exception)
+            {
+                if (retryForAvailability < 3)
+                {
+                    retryForAvailability++;
+                    var log = loggerFactory.CreateLogger<OrderContextSeed>();
+                    log.LogError(exception.Message);
+
+                    await SeedAsync(context, loggerFactory, retryForAvailability);
+                }
+            }
+        }
+
+        private static IEnumerable<Order> GetPerConfigOrders()
+        {
+            return new List<Order>()
+            {
+                new Order()
+                {
+                    AddressLine = "kashan",
+                    UserName = "Ali",
+                    FirstName = "Ali",
+                    LastName = "Chavoshi",
+                    EmailAddress = "Alichavoshii1372@gamil.com",
+                    Country = "Iran"
+                }
+            };
+        }
+    }
+}
