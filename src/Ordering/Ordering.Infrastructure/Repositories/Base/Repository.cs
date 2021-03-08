b1d8c3287f1d46771ab578d7fd499558c02b0331
Ali Chavoshi <alichavoshii1372@gamil.com>
2021-03-08 11:02:40 +0330

    Repository Implementation Developments with using EF.Core Context Objects

---
+++ src/Ordering/Ordering.Infrastructure/Repositories/Base/Repository.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Ordering.Core.Entities.Base;
+using Ordering.Core.Repositories.Base;
+using Ordering.Infrastructure.Data;
+
+namespace Ordering.Infrastructure.Repositories.Base
+{
+    public class Repository<T> : IRepository<T> where T : Entity
+    {
+        protected readonly OrderContext Context;
+
+        public Repository(OrderContext context)
+        {
+            Context = context;
+        }
+
+        public async Task<IReadOnlyList<T>> GetAllAsync()
+        {
+            return await Context.Set<T>().ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await Context.Set<T>().Where(predicate).ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeString = null,
+            bool disableTracking = true)
+        {
+            IQueryable<T> queryable = Context.Set<T>();
+            if (disableTracking)
+                queryable = queryable.AsNoTracking();
+            if (!string.IsNullOrWhiteSpace(includeString))
+                queryable = queryable.Include(includeString);
+            if (predicate != null)
+                queryable = queryable.Where(predicate);
+            if (orderBy != null)
+                return await orderBy(queryable).ToListAsync();
+            return await queryable.ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            List<Expression<Func<T, object>>> includes = null, string includeString = null, bool disableTracking = true)
+        {
+            IQueryable<T> queryable = Context.Set<T>();
+            if (disableTracking)
+                queryable = queryable.AsNoTracking();
+            if (includes != null)
+                queryable = includes.Aggregate(queryable, (current, include) => current.Include(include));
+            if (!string.IsNullOrWhiteSpace(includeString))
+                queryable = queryable.Include(includeString);
+            if (predicate != null)
+                queryable = queryable.Where(predicate);
+            if (orderBy != null)
+                return await orderBy(queryable).ToListAsync();
+            return await queryable.ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await Context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            await Context.Set<T>().AddAsync(entity);
+            await Context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            Context.Entry(entity).State = EntityState.Modified;
+            await Context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            Context.Set<T>().Remove(entity);
+            await Context.SaveChangesAsync();
+        }
+    }
+}
---
+++ src/Ordering/Ordering.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Ordering.Core.Entities;
+using Ordering.Core.Repositories;
+using Ordering.Infrastructure.Data;
+using Ordering.Infrastructure.Repositories.Base;
+
+namespace Ordering.Infrastructure.Repositories
+{
+    public class OrderRepository : Repository<Order>,IOrderRepository
+    {
+        public OrderRepository(OrderContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<Order>> GetOrderByUserName(string username)
+        {
+            return await Context.Order.Where(x => x.UserName == username).ToListAsync();
+        }
+    }
+}
