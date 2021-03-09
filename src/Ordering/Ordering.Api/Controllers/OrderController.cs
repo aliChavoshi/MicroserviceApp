760ee604783685302b9c18bb218c1e14310df1c2
Ali Chavoshi <alichavoshii1372@gamil.com>
2021-03-09 11:34:21 +0330

    Developing API Controller for Ordering Microservices

--- src/Ordering/Ordering.Api/Controllers/OrderController.cs
+++ src/Ordering/Ordering.Api/Controllers/OrderController.cs
@@ -2,7 +2,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using MediatR;
+using Ordering.Application.Commands;
+using Ordering.Application.Queries;
+using Ordering.Application.Responses;
+using Ordering.Core.Entities;
 
 namespace Ordering.Api.Controllers
 {
@@ -10,9 +16,29 @@ namespace Ordering.Api.Controllers
     [Route("api/v1/[Controller]/[action]")]
     public class OrderController : Controller
     {
-        public IActionResult Index()
+        private readonly IMediator _mediator;
+
+        public OrderController(IMediator mediator)
         {
-            return View();
+            _mediator = mediator;
         }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<OrderResponse>), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetOrdersByUsername(string userName)
+        {
+            var query = new GetOrderByUserNameQuery(userName);
+            var orders = await _mediator.Send(request: query);
+            return Ok(orders);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(OrderResponse), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> CheckoutOrder([FromBody] CheckoutOrderCommand command)
+        {
+            var result = await _mediator.Send(request: command);
+            return Ok(result);
+        }
+
     }
 }
