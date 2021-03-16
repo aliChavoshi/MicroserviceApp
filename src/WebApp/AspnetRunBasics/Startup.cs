bf0addd221f9d2666e89a33a9e0d2887470226d2
Ali Chavoshi <alichavoshii1372@gamil.com>
2021-03-16 12:33:09 +0330

    Settings Implementation for appsettings.json File

--- src/WebApp/AspnetRunBasics/Startup.cs
+++ src/WebApp/AspnetRunBasics/Startup.cs
@@ -1,11 +1,11 @@
-using AspnetRunBasics.Data;
-using AspnetRunBasics.Repositories;
+using AspnetRunBasics.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 
 namespace AspnetRunBasics
 {
@@ -21,26 +21,11 @@ namespace AspnetRunBasics
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            #region database services
-
-            //// use in-memory database
-            //services.AddDbContext<AspnetRunContext>(c =>
-            //    c.UseInMemoryDatabase("AspnetRunConnection"));
-
-            // add database dependecy
-            services.AddDbContext<AspnetRunContext>(c =>
-                c.UseSqlServer(Configuration.GetConnectionString("AspnetRunConnection")));
-
-            #endregion            
-
-            #region project services
-
-            // add repository dependecy
-            services.AddScoped<IProductRepository, ProductRepository>();
-            services.AddScoped<ICartRepository, CartRepository>();
-            services.AddScoped<IOrderRepository, OrderRepository>();
-            services.AddScoped<IContactRepository, ContactRepository>();
-
+            #region ApiSettings
+            services.Configure<ApiSettings>(
+                Configuration.GetSection(nameof(ApiSettings)));
+            services.AddSingleton<IApiSettings>(
+                sp => sp.GetRequiredService<IOptions<ApiSettings>>().Value);
             #endregion
 
             services.AddRazorPages();
