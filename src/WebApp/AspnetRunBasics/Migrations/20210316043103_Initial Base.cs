c996492224a678fb5e0151200728db46879c6d10
Ali Chavoshi <alichavoshii1372@gamil.com>
2021-03-16 10:21:53 +0330

    Add Asp Net Basic Solution

---
+++ src/WebApp/AspnetRunBasics/Migrations/20210316043103_Initial Base.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AspnetRunBasics.Migrations
+{
+    public partial class InitialBase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
